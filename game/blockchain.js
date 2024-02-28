1f61ca86543d3fd18f0d1e03a2cec781fdbd18c6
web3santa <usefulstory35@gmail.com>
2024-02-29 00:20:54 +0900

    finished

--- game/blockchain.js
+++ game/blockchain.js
@@ -1,32 +1,32 @@
-// let provider = new ethers.providers.Web3Provider(window.ethereum)
-// let signer
+let provider = new ethers.providers.Web3Provider(window.ethereum)
+let signer
 
-// async function connectMetamask() {
-//     await provider.send("eth_requestAccounts", []);
-//     signer = await provider.getSigner();
-//     console.log("Account address:", await signer.getAddress());
+async function connectMetamask() {
+    await provider.send("eth_requestAccounts", []);
+    signer = await provider.getSigner();
+    console.log("Account address:", await signer.getAddress());
 
-//     const balance = await signer.getBalance()
-//     const convertToEth = 1e18;
-//     console.log("account's balance in ether:", balance.toString() / convertToEth);
-// }
+    const balance = await signer.getBalance()
+    const convertToEth = 1e18;
+    console.log("account's balance in ether:", balance.toString() / convertToEth);
+}
 
-// async function claimTokens() {
-//     const runTokenContractAddress = "0x00bcD49DcEaDbB4b9c7fF9E54a64CF2ad61ead61";
-//     const runTokenContractAbi = [
-//         "function mintTokens(address account, uint256 amount) public",
-//     ];
-//     const runTokenContract = new ethers.Contract(runTokenContractAddress, runTokenContractAbi, provider);
-//     let convertToWei = 1000000000
-//     let amountToClaim = window.totalGweiScore * convertToWei
-//     await runTokenContract.connect(signer).mintTokens(signer.getAddress(), amountToClaim.toString())
-// }
+async function claimTokens() {
+    const runTokenContractAddress = "0x3546E3f9b4cfa885AE22978d10842b467E336634";
+    const runTokenContractAbi = [
+        "function mintTokens(address account, uint256 amount) public",
+    ];
+    const runTokenContract = new ethers.Contract(runTokenContractAddress, runTokenContractAbi, provider);
+    let convertToWei = 1000000000
+    let amountToClaim = window.totalGweiScore * convertToWei
+    await runTokenContract.connect(signer).mintTokens(signer.getAddress(), amountToClaim.toString())
+}
 
-// async function claimNft() {
-//     const nftContractAddress = "0x093298F529abdfCdA505e5C80A896478Dc32841a";
-//     const mintContractAbi = [
-//         "function mint(uint256 amount) public",
-//     ];
-//     const nftContract = new ethers.Contract(nftContractAddress, mintContractAbi, provider);
-//     await nftContract.connect(signer).mint(window.totalNFTScore.toString())
-// }
+async function claimNft() {
+    const nftContractAddress = "0x3e29469928457Ec93848F6C876738b8019472607";
+    const mintContractAbi = [
+        "function mint(uint256 amount) public",
+    ];
+    const nftContract = new ethers.Contract(nftContractAddress, mintContractAbi, provider);
+    await nftContract.connect(signer).mint(window.totalNFTScore.toString())
+}
